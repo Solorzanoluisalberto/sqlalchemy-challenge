7dbf0753b2d1fa0a54995d3e77df6eb2bfd97b87
Luis Solorzano <Solorzanoluis@hotmail.com>
2021-03-06 00:36:53 -0500

    calculate , , and  for all dates greater than and equal to the start date

--- Routes.py
+++ Routes.py
@@ -34,11 +34,12 @@ app = Flask(__name__)
 def Home_page():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
+        f"Available Routes (replace <date> with date in yyyy-mm-dd format):<br>"
+        f"/api/v1.0/precipitation<br>"
         f"/api/v1.0/stations<br>"
         f"/api/v1.0/tobs<br>"
-        f"/api/v1.0/&lt;start&gt;/&gt;end&lt;"
+        f"/api/v1.0/&lt;date&gt;<br>"
+        f"/api/v1.0/&lt;date&gt;/&lt;date&gt;<br>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -68,27 +69,65 @@ def stations():
     session = Session(engine)
     #     """Return a list of passenger data including the name, age, and sex of each passenger"""
     # Query all stations
-    results = session.query(Station.name).dictinct
+    results = session.query(Station.station, Station.name).all()
     session.close()
-    all_stations = list(np.ravel(results))
-    return jsonify(all_stations)
+    Station_list = list(results)#list(np.ravel(results))
+#     Station_list = []
+#     for stat in results:
+#         Station_list.append(stat)
+    
+    return jsonify(Station_list)
 
 @app.route("/api/v1.0/tobs")
+# Return a JSON list of temperature observations (TOBS) for the previous year
 def tobs():
-    # Return a JSON list of stations from the dataset.
     session = Session(engine)
     dates = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     dates # datetime.date(2016, 8, 23)
-    #     """Return a list of passenger data including the name, age, and sex of each passenger"""
     # Query all stations
     results = session.query(Measurement.date, Measurement.tobs)\
-                .filter(Measurement.station == USC00519281, Measurement.date.between(dates, dt.date(2017, 8, 23)))\
+                .filter(Measurement.station == 'USC00519281', Measurement.date.between(dates, dt.date(2017, 8, 23)))\
                 .group_by(Measurement.date)\
                 .order_by(Measurement.date)
     session.close()
-    temp_obser_12 = list(np.ravel(results))
-    return jsonify(temp_obser_12)
+#     temp_obser_12 = list(np.ravel(results))
+    tobs_list = []
+    for tob in results:
+        tobs_list.append(tob)
+    return jsonify(tobs_list)
 
+# When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+# Start Day Route
+@app.route("/api/v1.0/<start_date>")
+def start(start_date):
+    session = Session(engine)
+    start_calc = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start_date).\
+    group_by(Measurement.date).all()
+    session.close()
+    # Convert List of Tuples Into Normal List
+    start_list = list(start_calc)
+    # Return JSON List calculate `TMIN`, `TAVG`, and `TMAX`
+    return jsonify(start_list)
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def range(start_date,end_date):
+    session = Session(engine)
+    range_calc = session.query(Measurement.date.label("Date"), func.min(Measurement.tobs).label("Min"),\
+                               func.avg(Measurement.tobs).label("Avg"), func.max(Measurement.tobs).label("Max")).\
+    filter(Measurement.date.between(start_date,end_date)).\
+    group_by(Measurement.date).all()
+    session.close()
+    range_list = []
+    # Convert List of Tuples Into Normal List
+#     range_list = list(range_calc)
+#     for date in range_calc:
+
+    for r in range_calc:
+        range_list.append(f'date: {r.Date} Min temperature:{r.Min} Avg temperature:{round(r.Avg,2)}, Max temperature: {r.Max}')
+  
+    # Return JSON List calculate `TMIN`, `TAVG`, and `TMAX` range
+    return jsonify(range_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
